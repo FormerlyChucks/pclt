07c3f5442837fe229a21de7cdd1e221799d40e22
dio <61555147+IThinkImOKAY@users.noreply.github.com>
2021-03-06 23:46:58 -0600

    Delete removefile.py

--- removefile.py
+++
@@ -1,8 +0,0 @@
-import argparse, traceback
-
-parser = argparse.ArgumentParser()
-parser.add_argument('-f', '--file', help="File to delete", required=True)
-args = parser.parse_args()
-
-def removefile():
-    os.remove(args.file)
