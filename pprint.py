76e0e4a4ef0b5c302e10d9f0e6014e9959e637d5
dio <61555147+IThinkImOKAY@users.noreply.github.com>
2021-03-06 23:46:47 -0600

    Delete pprint.py

--- pprint.py
+++
@@ -1,9 +0,0 @@
-import argparse, traceback, pprint, json
-
-parser = argparse.ArgumentParser()
-parser.add_argument('-f', '--file', help="file to print", required=True)
-args = parser.parse_args()
-
-def pprint():
-    with open(args.file, 'r') as f:
-        pprint.pprint(json.loads(f.read()))
