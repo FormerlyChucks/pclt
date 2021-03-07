8c0b611a397635d14bf1f502631cdece9495df66
dio <61555147+IThinkImOKAY@users.noreply.github.com>
2021-03-06 23:10:57 -0600

    Update download.py

--- download.py
+++ download.py
@@ -1,33 +1,9 @@
-import argparse, requests, traceback
+import argparse, downsyndrome
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-u', '--url', help="URL To Download", required=True)
 parser.add_argument('-n', '--name', help="Name Of File", required=True)
-parser.add_argument('-a', '--agent', help="User Agent", required=False)
 args = parser.parse_args()
 
-url = args.url
-name = args.name
-agent = args.agent
-
 def download():   
-    if agent is None:
-        try:
-            response = requests.get(url)
-            file = open(name, "wb")
-            file.write(response.content)
-            file.close()
-            print('Downloaded {0} as {1}'.format(url,name))
-        except Exception:
-            print(traceback.format_exc())
-            quit()
-    else:
-        try:
-            response = requests.get(url, headers = {'User-agent': '{}'.format(agent)})
-            file = open(name, "wb")
-            file.write(response.content)
-            file.close()
-            print('Downloaded {0} as {1}'.format(url,name))
-        except Exception:
-            print(traceback.format_exc())
-            quit()
+    downsyndrome.download(url=args.url,file_name=args.name)
