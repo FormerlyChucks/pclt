c086c3fb7e1f09ae7e716d8a5d5d517ee64a201e
dio <61555147+IThinkImOKAY@users.noreply.github.com>
2021-03-06 23:49:57 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,13 @@
 from setuptools import setup
 
-def readme():
-    with open('README.rst') as f:
-        return f.read()
-
-setup(
-    name="pclt",
-    py_modules=["download",
-		"pprint",
-		"pyecho",
-		"removefile",
-		"makefolder"],
-    entry_points={"console_scripts": ["download=download:download",
-				                      "pprint=pprint:pprint",
-				                      "removefile=removefile:removefile"]},
-    version="0.0.7",
-    description="Python Command Line Tools",
-    long_description=readme(),
-    author="diogenesjunior",
-    author_email="diogenesjunior@protonmail.com",
-    url="https://github.com/ilikepyt/pclt/",
-    install_requires=['requests','downsyndrome']
-)
+setup(author="diogenesjunior",
+      author_email="diogenesjunior@protonmail.com",
+      description="Python Command Line Tools",
+      entry_points={"console_scripts": ["dl=pclt:dl", "pp=pclt:pp", "rf=pclt:rf"]},
+      install_requires=['requests','downsyndrome'],
+      long_description=open('README.rst').read(),
+      name="pclt",
+      packages=['pclt'],
+      py_modules=["dl", "pp", "rf"],
+      url="https://github.com/ilikepyt/pclt/",
+      version="0.0.8")
