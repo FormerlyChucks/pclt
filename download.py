4d34d771a3c42599e5cece2e8003904d703d7fe4
dio <61555147+IThinkImOKAY@users.noreply.github.com>
2021-03-06 23:47:07 -0600

    Delete download.py

--- download.py
+++
@@ -1,9 +0,0 @@
-import argparse, downsyndrome
-
-parser = argparse.ArgumentParser()
-parser.add_argument('-u', '--url', help="URL To Download", required=True)
-parser.add_argument('-n', '--name', help="Name Of File", required=True)
-args = parser.parse_args()
-
-def download():   
-    downsyndrome.download(url=args.url,file_name=args.name)
