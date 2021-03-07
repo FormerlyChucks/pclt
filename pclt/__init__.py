d197c25e0b7405c9bc5af9210fbffef28fe3d683
dio <61555147+IThinkImOKAY@users.noreply.github.com>
2021-03-06 23:53:46 -0600

    Update __init__.py

--- pclt/__init__.py
+++ pclt/__init__.py
@@ -1,20 +1,19 @@
 import argparse, downsyndrome, json, pprint
 
+parser = argparse.ArgumentParser()
+
 def rf():
-    parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--file', help="File to delete", required=True)
     args = parser.parse_args()
     os.remove(args.file)
 
 def pp():
-    parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--file', help="File to print", required=True)
     args = parser.parse_args()
     with open(args.file, 'r') as f:
         pprint.pprint(json.loads(f.read()))
 
 def dl():
-    parser = argparse.ArgumentParser()
     parser.add_argument('-u', '--url', help="URL To Download", required=True)
     parser.add_argument('-n', '--name', help="Name Of File", required=True)
     args = parser.parse_args()
