ce1523da7fb786a57c5d775cfb43ada345574481
dio <61555147+IThinkImOKAY@users.noreply.github.com>
2021-03-06 23:57:23 -0600

    Update __init__.py

--- pclt/__init__.py
+++ pclt/__init__.py
@@ -10,8 +10,7 @@ def rf():
 def pp():
     parser.add_argument('-f', '--file', help="File to print", required=True)
     args = parser.parse_args()
-    with open(args.file, 'r') as f:
-        pprint.pprint(json.loads(f.read()))
+    pprint.pprint(json.loads(open(args.file, 'r').read()))
 
 def dl():
     parser.add_argument('-u', '--url', help="URL To Download", required=True)
