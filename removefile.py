5ef80950013dd7d4f04faa492d6977b0a8358992
dio <61555147+IThinkImOKAY@users.noreply.github.com>
2021-03-06 23:11:18 -0600

    Update removefile.py

--- removefile.py
+++ removefile.py
@@ -5,8 +5,4 @@ parser.add_argument('-f', '--file', help="File to delete", required=True)
 args = parser.parse_args()
 
 def removefile():
-    try:
-        os.remove(args.file)
-    except Exception:
-        print(traceback.format_exc())
-        quit()
+    os.remove(args.file)
