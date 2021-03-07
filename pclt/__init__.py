60604f2868ec5e7ad25af64caf4affd543bff83f
dio <61555147+IThinkImOKAY@users.noreply.github.com>
2021-03-06 23:52:56 -0600

    Update __init__.py

--- pclt/__init__.py
+++ pclt/__init__.py
@@ -1,4 +1,4 @@
-import argparse
+import argparse, downsyndrome, json, pprint
 
 def rf():
     parser = argparse.ArgumentParser()
@@ -7,15 +7,13 @@ def rf():
     os.remove(args.file)
 
 def pp():
-    import pprint, json
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--file', help="file to print", required=True)
+    parser.add_argument('-f', '--file', help="File to print", required=True)
     args = parser.parse_args()
     with open(args.file, 'r') as f:
         pprint.pprint(json.loads(f.read()))
 
 def dl():
-    import downsyndrome
     parser = argparse.ArgumentParser()
     parser.add_argument('-u', '--url', help="URL To Download", required=True)
     parser.add_argument('-n', '--name', help="Name Of File", required=True)
